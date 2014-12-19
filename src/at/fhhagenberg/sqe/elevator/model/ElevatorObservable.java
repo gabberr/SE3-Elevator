aebbecb1a8392a6ac56d71d95353d367e054e01d
gabberr <gaber.setina@gmail.com>
2014-12-19 11:27:34 +0100

    Observable values.

--- src/at/fhhagenberg/sqe/elevator/model/ElevatorObservable.java
+++ src/at/fhhagenberg/sqe/elevator/model/ElevatorObservable.java
@@ -1,5 +1,6 @@
 package at.fhhagenberg.sqe.elevator.model;
 
+import java.rmi.RemoteException;
 import java.util.Observable;
 
 import sqelevator.IElevator;
@@ -12,13 +13,42 @@ public class ElevatorObservable extends Observable {
 	}
 	
 	
-	public void readValues() {
+	public void readValues() throws RemoteException{
+		ElevatorData data = new ElevatorData();
 		Integer elevatorNumber = 0;
 		Integer floorId;
 		
 		
-		controller.getCommittedDirection(elevatorNumber);
-		controller.
+		Long clockTick = controller.getClockTick();
+		Integer numberOfElevators = controller.getElevatorNum();
+		Integer numberOfFloors = controller.getFloorNum();
+		Integer floorHeight = controller.getFloorHeight();
+		
+		Integer commitedDirection = controller.getCommittedDirection(elevatorNumber);
+		Integer target = controller.getTarget(elevatorNumber);
+//		data.setTarger(target)
+		
+		Integer speed = controller.getElevatorSpeed(elevatorNumber);
+		Integer position = controller.getElevatorPosition(elevatorNumber);
+		Integer accl = controller.getElevatorAccel(elevatorNumber);
+		Integer doorStatus = controller.getElevatorDoorStatus(elevatorNumber);
+		
+		Integer closestFloor = controller.getElevatorFloor(elevatorNumber);
+		
+		Integer capacity = controller.getElevatorCapacity(elevatorNumber);
+		Integer elevatorWeight =controller.getElevatorWeight(elevatorNumber);
+		
+		for (int i = 0; i < numberOfFloors; i++) {
+//			controller.get
+			
+		}
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 }
