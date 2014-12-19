5c1849b77b66991e0f403158dcff4534e8f09dcd
gabberr <gaber.setina@gmail.com>
2014-12-19 14:03:59 +0100

    Finished Observable.

--- src/at/fhhagenberg/sqe/elevator/model/ElevatorObservable.java
+++ src/at/fhhagenberg/sqe/elevator/model/ElevatorObservable.java
@@ -1,54 +1,81 @@
 package at.fhhagenberg.sqe.elevator.model;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.Observable;
 
 import sqelevator.IElevator;
 
 public class ElevatorObservable extends Observable {
-	
+
 	private IElevator controller;
-	public ElevatorObservable( IElevator _eif) {
-		controller= _eif;
+	private ElevatorData currentValue;
+
+	public ElevatorObservable(IElevator _eif) {
+		controller = _eif;
 	}
-	
-	
-	public void readValues() throws RemoteException{
-		ElevatorData data = new ElevatorData();
-		Integer elevatorNumber = 0;
-		Integer floorId;
-		
-		
-		Long clockTick = controller.getClockTick();
+
+	public void readValues() throws RemoteException {
+
 		Integer numberOfElevators = controller.getElevatorNum();
 		Integer numberOfFloors = controller.getFloorNum();
-		Integer floorHeight = controller.getFloorHeight();
-		
-		Integer commitedDirection = controller.getCommittedDirection(elevatorNumber);
-		Integer target = controller.getTarget(elevatorNumber);
-//		data.setTarger(target)
-		
-		Integer speed = controller.getElevatorSpeed(elevatorNumber);
-		Integer position = controller.getElevatorPosition(elevatorNumber);
-		Integer accl = controller.getElevatorAccel(elevatorNumber);
-		Integer doorStatus = controller.getElevatorDoorStatus(elevatorNumber);
-		
-		Integer closestFloor = controller.getElevatorFloor(elevatorNumber);
-		
-		Integer capacity = controller.getElevatorCapacity(elevatorNumber);
-		Integer elevatorWeight =controller.getElevatorWeight(elevatorNumber);
-		
-		for (int i = 0; i < numberOfFloors; i++) {
-//			controller.get
+
+		ElevatorData data = new ElevatorData();
+
+		for (int i = 0; i < numberOfElevators; i++) {
+
+			Integer elevatorNumber = 0;
+			Integer floorId;
+
+			Long clockTick = controller.getClockTick();
+
+			Integer floorHeight = controller.getFloorHeight();
+
+			Integer commitedDirection = controller
+					.getCommittedDirection(elevatorNumber);
+			Integer target = controller.getTarget(elevatorNumber);
 			
+			Integer direction = controller.getCommittedDirection(i);
+			Integer speed = controller.getElevatorSpeed(i);
+			Integer position = controller.getElevatorPosition(i);
+			Integer accl = controller.getElevatorAccel(i);
+			Integer doorStatus = controller
+					.getElevatorDoorStatus(elevatorNumber);
+
+			Integer closestFloor = controller.getElevatorFloor(i);
+			Integer capacity = controller.getElevatorCapacity(i);
+			Integer elevatorWeight = controller
+					.getElevatorWeight(i);
+			
+			ArrayList<Boolean> elevatorPressedButtons = new ArrayList<Boolean>();
+
+			for (int j = 0; j < numberOfFloors; j++) {
+
+				boolean val = controller.getElevatorButton(i, j);
+				elevatorPressedButtons.add(val);
+
+			}
+			data.addElevatorBox(direction, accl, doorStatus, closestFloor, position, speed, elevatorWeight, capacity, target, elevatorPressedButtons);
 		}
+
+		// floor information
+
+		ArrayList<Boolean> floorButtonsDown = new ArrayList<Boolean>();
+		ArrayList<Boolean> floorButtonsUp = new ArrayList<Boolean>();
+
+		for (int j = 0; j < numberOfFloors; j++) {
+
+			boolean down = controller.getFloorButtonDown(j);
+			boolean up = controller.getFloorButtonUp(j);
+			floorButtonsDown.add(down);
+			floorButtonsUp.add(up);
+
+		}
+		data.setFloorButtonsDown(floorButtonsDown);
+		data.setFloorButtonsUp(floorButtonsUp);
 		
-		
-		
-		
-		
-		
-		
+		currentValue = data;
+		notifyObservers(currentValue);
 		
 	}
 }
