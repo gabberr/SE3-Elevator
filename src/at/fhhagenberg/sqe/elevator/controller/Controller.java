bcddc807bfd8ee8156a59b3d351bd07fd65ff6b9
ilias koletsis <ilias.koletsis@gmail.com>
2014-12-19 09:40:01 +0100

    controller view...

--- src/at/fhhagenberg/sqe/elevator/controller/Controller.java
+++ src/at/fhhagenberg/sqe/elevator/controller/Controller.java
@@ -1,5 +1,11 @@
 package at.fhhagenberg.sqe.elevator.controller;
 
+import at.fhhagenberg.sqe.elevator.view.View;
+
 public class Controller {
 	View view = null;
+	
+	public Controller(View view){
+		this.view = view;
+	}
 }
