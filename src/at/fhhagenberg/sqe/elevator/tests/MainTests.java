3e758e53a87dc2c0bdfccad6f4a146a6092d0b5c
gabberr <gaber.setina@gmail.com>
2015-01-14 17:49:56 +0100

    created test package

---
+++ src/at/fhhagenberg/sqe/elevator/tests/MainTests.java
@@ -0,0 +1,14 @@
+package at.fhhagenberg.sqe.elevator.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
